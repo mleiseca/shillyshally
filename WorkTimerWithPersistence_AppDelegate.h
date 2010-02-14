28f7c32693db91373ad76463188fabed1da8d17e
mleiseca <mleiseca@localhost>
2010-02-14 14:56:24 -0600

    basic interface done. data model set. trying to use bindings to populate interface

---
+++ WorkTimerWithPersistence_AppDelegate.h
@@ -0,0 +1,28 @@
+//
+//  WorkTimerWithPersistence_AppDelegate.h
+//  WorkTimerWithPersistence
+//
+//  Created by Michael Leiseca on 2/14/10.
+//  Copyright Grubhub Inc 2010 . All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface WorkTimerWithPersistence_AppDelegate : NSObject 
+{
+    NSWindow *window;
+    
+    NSPersistentStoreCoordinator *persistentStoreCoordinator;
+    NSManagedObjectModel *managedObjectModel;
+    NSManagedObjectContext *managedObjectContext;
+}
+
+@property (nonatomic, retain) IBOutlet NSWindow *window;
+
+@property (nonatomic, retain, readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (nonatomic, retain, readonly) NSManagedObjectModel *managedObjectModel;
+@property (nonatomic, retain, readonly) NSManagedObjectContext *managedObjectContext;
+
+- (IBAction)saveAction:sender;
+
+@end
---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  WorkTimerWithPersistence
+//
+//  Created by Michael Leiseca on 2/14/10.
+//  Copyright Grubhub Inc 2010. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
